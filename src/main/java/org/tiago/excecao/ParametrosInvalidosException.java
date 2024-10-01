88330b325b3df8b35a01e666359b4a1661eb61bf
Tiago Silva <tiagobsb84@gmail.com>
2024-10-01 10:06:57 -0300

    Finalizando projeto Controle de Fluxo

---
+++ src/main/java/org/tiago/excecao/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+package org.tiago.excecao;
+
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+}
